3ac4b6170989e9d9feeebcfe82f5cb0513b937f5
Kirill Zolottsev <carmine@atls.team>
2022-11-10 12:21:46 +0400

    chore: types

--- shared/utils/src/helpers/formatted-rating.util.ts
+++ shared/utils/src/helpers/formatted-rating.util.ts
@@ -1,4 +1,4 @@
-const formattedRating = (rating) => {
+const formattedRating = (rating: number): number | string => {
   if (rating === 0) return '-'
   if (rating.toString().length === 1) return `${rating}.0`
 
